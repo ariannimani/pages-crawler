a7b6cc5d767d3959875de643f828e69d3fcbee90
aarriiaan <aarriiaann@gmail.com>
2023-09-24 13:54:12 +0200

    update

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Theme from '../../theme';
 import { Pages } from '../../components';
 import Header from '../../components/Header/Header';
-import MainHeader from '../../components/MainHeader/MainHeader';
 
 import './Home.scss';
 
